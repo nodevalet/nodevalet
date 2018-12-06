fea8af711cfd8434e930de47b3cbef3c8dc26aed
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-05 15:13:01 -0900

    Update snoice.sh

--- ak/snoice.sh
+++ ak/snoice.sh
@@ -23,9 +23,9 @@ darkgray='\033[1;30m'  # dark gray
 black='\033[0;30m'  # black
 nocolor='\033[0m'    # no color
 
-printf -e "..%s.." "${lightred}"
-printf -e "..%s.." "${lightgreen}"
-printf -e "..%s.." "${nocolor}"
+printf "..%s.." "$lightred"
+printf "..%s.." "$lightgreen"
+printf "..%s.." "$nocolor"
 
 # Set Vars
 LOGFILE='/var/log/logjammin.log'
@@ -33,14 +33,14 @@ LOGFILE='/var/log/logjammin.log'
 
 function begin_log() {
 # Create Log File and Begin
-printf -e "..%s.." "${lightcyan}"
+printf "..%s.." "$lightcyan"
 rm -rf $LOGFILE
 echo -e "---------------------------------------------------- " | tee -a "$LOGFILE"
 echo -e " `date +%m.%d.%Y_%H:%M:%S` : SCRIPT STARTED SUCCESSFULLY " | tee -a "$LOGFILE"
 echo -e "---------------------------------------------------- " | tee -a "$LOGFILE"
 echo -e "---------- AKcryptoGUY's Testing Script ------------ " | tee -a "$LOGFILE"
 echo -e "---------------------------------------------------- \n" | tee -a "$LOGFILE"
-printf -e "..%s.." "${nocolor}"
+printf "..%s.." "$nocolor"
 sleep 1
 }
 
