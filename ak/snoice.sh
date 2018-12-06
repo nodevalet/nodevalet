9ed45fb2d42828cf810ba4e35d106c509d009c85
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-05 18:56:10 -0900

    Update snoice.sh

--- ak/snoice.sh
+++ ak/snoice.sh
@@ -70,7 +70,7 @@ Read -p "How many private keys would you like me to generate, boss?" GENKEYS
 
 #!/bin/bash
 # Tested using bash version 4.1.5
-for ((i=1;i<=100;i++)); 
+for ((i=1;i<=GENKEYS;i++)); 
 do 
    # your-unix-command-here
    echo $i
