843f0fdd17ac375fc94c0ed8dcf49f7a7cd09445
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-05 18:16:20 -0900

    get "blocks" for current masternode

--- ak/snoice.sh
+++ ak/snoice.sh
@@ -54,7 +54,7 @@ apt-get -qqy -o=Dpkg::Use-Pty=0 -o=Acquire::ForceIPv4=true install figlet shellc
 
 echo "/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf getinfo"  | tee -a "$LOGFILE"
 
-/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf getinfo  | tee -a "$LOGFILE"
+/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf getinfo  | tee -a /var/helium/getinfo_n1
 
 
 echo "/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf masternode genkey"   | tee -a "$LOGFILE"
@@ -68,7 +68,8 @@ echo "Saving genkey to /var/helium/genkey1"  | tee -a "$LOGFILE"
 touch /var/helium/genkey1  | tee -a "$LOGFILE"
 /usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf masternode genkey >> /var/helium/genkey1   | tee -a "$LOGFILE"
 
-
+echo "grep "blocks" /var/helium_getinfo_n1"
+grep "blocks" /var/helium_getinfo_n1
 
 
 
