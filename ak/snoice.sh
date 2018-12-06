9fa0e054ce56aaa03c9736b6445966b664aa3205
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-05 18:46:04 -0900

    fun with private keys

--- ak/snoice.sh
+++ ak/snoice.sh
@@ -68,6 +68,16 @@ echo "Saving genkey to /var/helium/genkey1"  | tee -a "$LOGFILE"
 
 touch /var/helium/genkey1  | tee -a "$LOGFILE"
 /usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf masternode genkey >> /var/helium/genkey1   | tee -a "$LOGFILE"
+/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf masternode genkey >> /var/helium/genkey1   | tee -a "$LOGFILE"
+/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf masternode genkey >> /var/helium/genkey1   | tee -a "$LOGFILE"
+/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf masternode genkey >> /var/helium/genkey1   | tee -a "$LOGFILE"
+/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf masternode genkey >> /var/helium/genkey1   | tee -a "$LOGFILE"
+/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf masternode genkey >> /var/helium/genkey1   | tee -a "$LOGFILE"
+/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf masternode genkey >> /var/helium/genkey1   | tee -a "$LOGFILE"
+/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf masternode genkey >> /var/helium/genkey1   | tee -a "$LOGFILE"
+/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf masternode genkey >> /var/helium/genkey1   | tee -a "$LOGFILE"
+/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf masternode genkey >> /var/helium/genkey1   | tee -a "$LOGFILE"
+
 
 echo "grep "blocks" /var/helium/getinfo_n1" 
 
@@ -78,7 +88,21 @@ echo "$BLOCKS2"
 echo "${BLOCKS2//[^0-9]/}"
 echo "${BLOCKS3//[^0-9]/}"
 
+echo -e "\n"
+echo -e /var/helium/genkey1
 
+PRIVKEY1=$(sed -n 1p /var/helium/genkey1)
+PRIVKEY2=$(sed -n 2p /var/helium/genkey1)
+PRIVKEY3=$(sed -n 3p /var/helium/genkey1)
+PRIVKEY4=$(sed -n 4p /var/helium/genkey1)
+PRIVKEY5=$(sed -n 5p /var/helium/genkey1)
+
+echo -e "\n"
+echo -e "First private key $PRIVKEY1"
+echo -e "Second private key $PRIVKEY2"
+echo -e "Third private key $PRIVKEY3"
+echo -e "Fourth private key $PRIVKEY4"
+echo -e "Fifth private key $PRIVKEY5"
 
 
 
