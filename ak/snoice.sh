821cc1fcbb41c0f2f7ffd906ebce84db3fdda23f
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-05 22:18:32 -0900

    Update snoice.sh

--- ak/snoice.sh
+++ ak/snoice.sh
@@ -118,7 +118,6 @@ NEWEST=${LTRIMTIME%%,*}
 TIMEDIF=$(echo -e "$((`date +%s`-$NEWEST))")
 echo -e "This masternode is $TIMEDIF seconds behind the latest block." 
    #check if current
-   echo -e "TIMEDIF is set to $TIMEDIF"
    if (($TIMEDIF <= 60 && $TIMEDIF >= -60))
 	then echo -e "The blockchain is synced"
 	else echo -e "That's the same as $(((`date +%s`-$NEWEST)/60)) minutes or $(((`date +%s`-$NEWEST)/3600)) hours behind."
