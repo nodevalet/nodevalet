94c6b77df52ef332e9133a08a106cec778ad3c61
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-11 16:59:26 +0000

    contents

--- autoupdate/updatefromsource.sh
+++ autoupdate/updatefromsource.sh
@@ -1,1 +1,22 @@
-#check for updates and build from source if installing binaries failed.
+#check for updates and build from source if installing binaries failed. 
+
+cd /root/installtemp
+GITAPI_URL="https://api.github.com/repos/heliumchain/helium/releases/latest"
+CURVERSION=`cat currentversion`
+NEWVERSION="$(curl -s $GITAPI_URL | grep tag_name)"
+if [ "$CURVERSION" != "$NEWVERSION" ]
+then systemctl stop 'helium*'
+	git clone https://github.com/heliumchain/helium.git
+	cd helium
+	./autogen.sh
+	./configure --disable-dependency-tracking --enable-tests=no --without-gui --without-miniupnpc --with-incompatible-bdb CFLAGS="-march=native" LIBS="-lcurl -lssl -lcrypto -lz"
+	make
+	make install
+        cd /usr/local/bin && rm -f !"("activate_masternodes_helium")"
+	cp /root/installtemp/helium/src/{helium-cli,heliumd,helium-tx} /usr/local/bin/
+        rm -rf ~/helium
+        cd /root/installtemp
+        curl -s $GITAPI_URL \
+             | grep tag_name > currentversion
+        reboot
+fi
