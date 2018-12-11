7ad6e9982f54c2f24cf3cee63b8e8e487972a925
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-10 23:37:54 -0900

    remove synccheck code

--- silentinstall.sh
+++ silentinstall.sh
@@ -253,71 +253,6 @@ done
 fi
  }
 
-function get_blocks() {
-# echo "grep "blocks" $INSTALLDIR/getinfo_n1" 
-BLOCKS=$(grep "blocks" $INSTALLDIR/getinfo_n1 | tr -dc '0-9')
-echo -e "Masternode 1 is currently synced through block $BLOCKS.\n"
-}
-
-function check_blocksync() {
-# set SECONDS+XXXXX to however long is reasonable to let the initial
-# chain sync continue before reporting an error back to the user
-end=$((SECONDS+7200))
-
-while [ $SECONDS -lt $end ]; do
-    echo -e "Time $SECONDS"
-    
-	rm -rf $INSTALLDIR/getinfo_n1
-	touch $INSTALLDIR/getinfo_n1
-	/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf getinfo  | tee -a $INSTALLDIR/getinfo_n1
-	clear
-    
-    # if  masternode not running, echo masternode not running and break
-    BLOCKS=$(grep "blocks" $INSTALLDIR/getinfo_n1 | tr -dc '0-9')
-    echo -e "$BLOCKS is the current number of blocks"
-    
-    if (($BLOCKS <= 1 )) ; then echo "Masternode is not syncing" ; break
-    else sync_check
-    fi
-    
-    if [ "$SYNCED" = "yes" ]; then printf "${lightcyan}" ; echo "Masternode synced" ; printf "${nocolor}" ; break
-    else echo -e "Blockchain not synced; will check again in 5 seconds\n"
-    sleep 5
-    fi
-done
-
-    if [ "$SYNCED" = "no" ]; then printf "${lightred}" ; echo "Masternode did not sync in allowed time" ; printf "${nocolor}"
-    # radio home that blockchain sync was unsuccessful
-    # add curl here
-    else : ; fi
-
-echo -e "All done."
-}
-
-function sync_check() {
-CNT=`/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf getblockcount`
-# echo -e "CNT is set to $CNT"
-HASH=`/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf getblockhash ${CNT}`
-#echo -e "HASH is set to $HASH"
-TIMELINE1=`/usr/local/bin/helium-cli -conf=/etc/masternodes/helium_n1.conf getblock ${HASH} | grep '"time"'`
-TIMELINE=$(echo $TIMELINE1 | tr -dc '0-9')
-BLOCKS=$(grep "blocks" $INSTALLDIR/getinfo_n1 | tr -dc '0-9')
-# echo -e "TIMELINE is set to $TIMELINE"
-LTRIMTIME=${TIMELINE#*time\" : }
-# echo -e "LTRIMTIME is set to $LTRIMTIME"
-NEWEST=${LTRIMTIME%%,*}
-# echo -e "NEWEST is set to $NEWEST"
-TIMEDIF=$(echo -e "$((`date +%s`-$NEWEST))")
-echo -e "This masternode is $TIMEDIF seconds behind the latest block." 
-   #check if current
-   if (($TIMEDIF <= 60 && $TIMEDIF >= -60))
-	then echo -e "The blockchain is almost certainly synced.\n"
-	SYNCED="yes"
-	else echo -e "That's the same as $(((`date +%s`-$NEWEST)/3600)) hours or $(((`date +%s`-$NEWEST)/86400)) days behind.\n"
-	SYNCED="no"
-   fi	
-}
-
 function install_binaries() {
 #check for binaries and install if found   
 
@@ -368,8 +303,5 @@ touch /root/vpsvaletreboot.txt
 chmod 0700 /root/code-red/postinstall_api.sh
 curl -X POST https://www.heliumstats.online/code-red/status.php -H 'Content-Type: application/json-rpc' -d '{"hostname":"'"$HNAME"'","message": "Restarting Server to finalize installation..."}'
 restart_server
-
-# check_blocksync
-# sync_check
 
 echo -e "Log of events saved to: $LOGFILE \n"
