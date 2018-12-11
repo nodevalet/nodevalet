4ce4af62ea9442f684e47e7bf8f40735620f0f80
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-11 22:29:34 +0000

    stop daemon before trying to overwrite

--- autoupdate/updatebinaries.sh
+++ autoupdate/updatebinaries.sh
@@ -5,7 +5,8 @@ GITAPI_URL="https://api.github.com/repos/heliumchain/helium/releases/latest"
 CURVERSION=`cat currentversion`
 NEWVERSION="$(curl -s $GITAPI_URL | grep tag_name)"
 if [ "$CURVERSION" != "$NEWVERSION" ]
-then curl -s $GITAPI_URL \
+then systemctl stop 'helium*' \
+		| curl -s $GITAPI_URL \
 		| grep browser_download_url \
   		| grep x86_64-linux-gnu.tar.gz \
   		| cut -d '"' -f 4 \
