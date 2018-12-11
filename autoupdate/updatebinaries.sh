69933d33cd23dbb79bcccf3c78252b115b44a2c2
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-11 15:05:54 +0000

    contents of autoupdate.sh&more tidy

---
+++ autoupdate/updatebinaries.sh
@@ -0,0 +1,23 @@
+
+#!/bin/bash
+#check for updates and install if necessary
+
+GITAPI_URL="https://api.github.com/repos/heliumchain/helium/releases/latest"
+CURVERSION=`cat currentversion`
+NEWVERSION="$(curl -s $GITAPI_URL | grep tag_name)"
+if [ "$CURVERSION" != "$NEWVERSION" ]
+then curl -s $GITAPI_URL \
+      | grep browser_download_url \
+  		| grep x86_64-linux-gnu.tar.gz \
+  		| cut -d '"' -f 4 \
+  		| wget -qi -
+	TARBALL="$(find . -name "*x86_64-linux-gnu.tar.gz")"
+	tar -xzf $TARBALL
+	EXTRACTDIR=${TARBALL%-x86_64-linux-gnu.tar.gz}
+  cp -r $EXTRACTDIR/bin/. /usr/local/bin/
+  curl -s $GITAPI_URL \
+             | grep tag_name > currentversion
+	rm -r $EXTRACTDIR
+	rm -f $TARBALL
+	reboot
+fi
