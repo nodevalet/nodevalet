52c7ab75cdc9ba0ec2eb73b10a4c8284e0c4bf74
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-10 19:57:44 -0900

    Fix final transmission

--- postinstall_api.sh
+++ postinstall_api.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-# Set Variables
-INSTALLDIR='/root/installtemp'
-LOGFILE='/root/installtemp/silentinstall.log'
-
-echo -e "Server has restarted after masternode install"  | tee -a "$LOGFILE"
-    
-# set hostname variable to the name planted by install script
-	if [ -e $INSTALLDIR/vpshostname.info ]
-	then HNAME=$(<$INSTALLDIR/vpshostname.info)
-	echo -e "vpshostname.info found, setting HNAME to $HNAME"  | tee -a "$LOGFILE"
-	else HNAME=`hostname`
-	echo -e "vpshostname.info not found, setting HNAME to $HNAME"  | tee -a "$LOGFILE"
-fi
+function final_message() {
 
 if [ -e /root/vpsvaletreboot.txt ]; then
-	echo -e "Reporting Reboot complete to mothership"  | tee -a "$LOGFILE"
-	curl -X POST https://www.heliumstats.online/code-red/status.php -H 'Content-Type: application/json-rpc' -d '{"hostname":"'"$HNAME"'","message":"VPS Restart Complete; installation success"}'
-	rm /root/vpsvaletreboot.txt
-	#rm -rf /root/installtemp
+	# Set Variables
+	INSTALLDIR='/root/installtemp'
+	LOGFILE='/root/installtemp/silentinstall.log'
+	TRANSMITMN=`cat /root/installtemp/masternode.return`
+
+	# set hostname variable to the name planted by install script
+	if [ -e $INSTALLDIR/vpshostname.info ]
+	then HNAME=$(<$INSTALLDIR/vpshostname.info)
+	else HNAME=`hostname`
+	fi
+
+	# log successful reboot
+	echo -e "Server has restarted after masternode install"  | tee -a "$LOGFILE"
+	echo -e "Sending masternode.return data to mothership"  | tee -a "$LOGFILE"
+	# transmit masternode.return to mothership
+	curl -X POST https://www.heliumstats.online/code-red/status.php -H 'Content-Type: application/json-rpc' -d '{"hostname":"'"$HNAME"'","message": "'"$TRANSMITMN"'"}'
+
+# Add a sequence to interpret the reply as success or fail $?
+rm /root/vpsvaletreboot.txt
+
+# add code to clean up the rest of unnecessary files; keep the log and masternode files except for the mnaddresses
+# rm -rf /root/installtemp
+
+else :
 fi
+}
+
+final_message
+
+exit
