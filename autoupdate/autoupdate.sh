06ec1152e960b9e6c7aee4f3631bb17ea4005bd1
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-11 18:25:59 +0000

    content

--- autoupdate/autoupdate.sh
+++ autoupdate/autoupdate.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-# runs updatebinaries and, if that fails, runs updatefromsource
+# to be added to crontab to run updatebinaries and, if that fails, run updatefromsource
+
+bash updatebinaries.sh || bash updatefromsource.sh
