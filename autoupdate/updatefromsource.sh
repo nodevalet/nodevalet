48ea42037bf7362f6e882e71fa52cbb13a3bd366
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-11 18:28:21 +0000

    fixed paths and added fail condition

--- autoupdate/updatefromsource.sh
+++ autoupdate/updatefromsource.sh
@@ -1,5 +1,5 @@
 #check for updates and build from source if installing binaries failed. 
-
+ 
 cd /root/installtemp
 GITAPI_URL="https://api.github.com/repos/heliumchain/helium/releases/latest"
 CURVERSION=`cat currentversion`
@@ -10,13 +10,12 @@ then systemctl stop 'helium*'
 	cd helium
 	./autogen.sh
 	./configure --disable-dependency-tracking --enable-tests=no --without-gui --without-miniupnpc --with-incompatible-bdb CFLAGS="-march=native" LIBS="-lcurl -lssl -lcrypto -lz"
-	make
-	make install
-	cd /usr/local/bin && rm -f !"("activate_masternodes_helium")"
+	make && make install
+        cd /usr/local/bin && rm -f !"("activate_masternodes_helium")"
 	cp /root/installtemp/helium/src/{helium-cli,heliumd,helium-tx} /usr/local/bin/
-	rm -rf ~/helium
-	cd /root/installtemp
-	curl -s $GITAPI_URL \
-		| grep tag_name > currentversion
-	reboot
+        rm -rf ~/helium
+        cd /root/installtemp
+        curl -s $GITAPI_URL \
+             | grep tag_name > currentversion
+        reboot
 fi
