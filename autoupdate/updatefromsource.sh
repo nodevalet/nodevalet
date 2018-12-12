3b12770264e40892cd055c48586276d6fd0cc625
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-12 00:54:13 +0000

    try fail again

--- autoupdate/updatefromsource.sh
+++ autoupdate/updatefromsource.sh
@@ -5,17 +5,18 @@ GITAPI_URL="https://api.github.com/repos/heliumchain/helium/releases/latest"
 CURVERSION=`cat currentversion`
 NEWVERSION="$(curl -s $GITAPI_URL | grep tag_name)"
 if [ "$CURVERSION" != "$NEWVERSION" ]
-then systemctl stop 'helium*'
-	git clone https://github.com/heliumchain/helium.git
-	cd helium
-	./autogen.sh
-	./configure --disable-dependency-tracking --enable-tests=no --without-gui --without-miniupnpc --with-incompatible-bdb CFLAGS="-march=native" LIBS="-lcurl -lssl -lcrypto -lz"
-	make && make install
-        cd /usr/local/bin && rm -f !"("activate_masternodes_helium")"
-	cp /root/installtemp/helium/src/{helium-cli,heliumd,helium-tx} /usr/local/bin/
-        rm -rf root/installtemp/helium
-        cd /root/installtemp
-        curl -s $GITAPI_URL \
-             | grep tag_name > currentversion
-        reboot
+then systemctl stop 'helium*' \
+	&& git clone https://github.com/heliumchain/helium.git \
+	&& cd helium \
+	&& ./autogen.sh \
+	&& ./configure --disable-dependency-tracking --enable-tests=no --without-gui --without-miniupnpc --with-incompatible-bdb CFLAGS="-march=native" LIBS="-lcurl -lssl -lcrypto -lz" \
+	&& make \
+	&& make install \
+        && cd /usr/local/bin && rm -f !"("activate_masternodes_helium")" \
+	&& cp /root/installtemp/helium/src/{helium-cli,heliumd,helium-tx} /usr/local/bin/ \
+        && rm -rf root/installtemp/helium \
+        && cd /root/installtemp \
+        && curl -s $GITAPI_URL \
+             | grep tag_name > currentversion \
+        && reboot
 fi
