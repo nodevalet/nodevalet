9df8a1bca0cf5726f5b18b34502d150b30b91037
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-12 21:47:51 -0900

    force ip4

--- ksplice.sh
+++ ksplice.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 # install Ksplice Uptrack
 
-sudo apt-get install libgtk2-perl consolekit iproute libck-connector0 libcroco3 libglade2-0 libpam-ck-connector librsvg2-2 librsvg2-common python-cairo python-dbus python-gi python-glade2 python-gobject-2 python-gtk2 python-pycurl python-yaml dbus-x11 -y
+sudo apt-get -o=Dpkg::Use-Pty=0 -o=Acquire::ForceIPv4=true install libgtk2-perl consolekit iproute libck-connector0 libcroco3 libglade2-0 libpam-ck-connector librsvg2-2 librsvg2-common python-cairo python-dbus python-gi python-glade2 python-gobject-2 python-gtk2 python-pycurl python-yaml dbus-x11 -y
 sudo wget https://ksplice.oracle.com/uptrack/dist/xenial/ksplice-uptrack.deb
 sudo dpkg -i ksplice-uptrack.deb
 sudo sed -i "s/autoinstall = no/autoinstall = yes/" /etc/uptrack/uptrack.conf
