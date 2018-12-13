8028f103ae6b62539a75e527c95273a3aeb4ad49
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-13 07:18:12 -0900

    add logging

--- autoupdate/autoupdate.sh
+++ autoupdate/autoupdate.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # to be added to crontab to run updatebinaries and, if that fails, run updatefromsource, if that fails, restarts daemon and try again tomorrow.
 
+LOGFILE='/root/installtemp/autoupdate.log'
+
+echo -e "Running autoupdate against installed wallet binaries." | tee -a "$LOGFILE"
+date | tee -a "$LOGFILE"
+
 bash /root/code-red/autoupdate/updatebinaries.sh || bash /root/code-red/autoupdate/updatefromsource.sh || cd /usr/local/bin && ./activate_masternodes_helium
