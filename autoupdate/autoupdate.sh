22d4a3e54e1451dbfc187988fdadc8178f17a579
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-12 23:59:10 -0900

    add full path to files

--- autoupdate/autoupdate.sh
+++ autoupdate/autoupdate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # to be added to crontab to run updatebinaries and, if that fails, run updatefromsource, if that fails, restarts daemon and try again tomorrow.
 
-bash updatebinaries.sh || bash updatefromsource.sh || cd /usr/local/bin && ./activate_masternodes_helium
+bash /root/code-red/autoupdate/updatebinaries.sh || bash /root/code-red/autoupdate/updatefromsource.sh || cd /usr/local/bin && ./activate_masternodes_helium
