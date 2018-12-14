0941a7617546c1edea9b43c264a81c173f3eca56
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-14 13:11:20 +0000

    fixed syntax

--- maintenance/makerun.sh
+++ maintenance/makerun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #compare nr. of running nodes to number of installed nodes. If different restart daemon
+# Add the following to the crontab (i.e. crontab -e)
+# */5 * * * * /root/code-red/maintenance/makerun.sh
 
 INSTALLDIR='/root/installtemp'
 PROJECT=`cat $INSTALLDIR/vpscoin.info`
@@ -8,5 +10,5 @@ CUR_DAEMON=`expr $TOTAL - 1`
 EXP_DAEMON=`cat /root/installtemp/vpsnumber.info`
 if [ "$CUR_DAEMON" != "$EXP_DAEMON" ]
   then cd /usr/local/bin
-  ./activate_masternodes_helium
+  ./activate_masternodes_"$PROJECT"
 fi
