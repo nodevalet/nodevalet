d8e430336d88089e732903903d265fead63cfe6e
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-14 07:35:42 +0000

    install packages only if updating from source

--- autoupdate/updatefromsource.sh
+++ autoupdate/updatefromsource.sh
@@ -12,7 +12,13 @@ if [ "$CURVERSION" != "$NEWVERSION" ]
 then echo -e "Installed version is $CURVERSION; new version detected: $NEWVERSION" | tee -a "$LOGFILE"
 	echo -e "I couldn't download the new binaries, so I am now" | tee -a "$LOGFILE"
 	echo -e "attempting to build new wallet version from source" | tee -a "$LOGFILE"
-	
+	add-apt-repository -yu ppa:bitcoin/bitcoin
+	apt-get -qq -o=Dpkg::Use-Pty=0 -o=Acquire::ForceIPv4=true update
+	apt-get -qqy -o=Dpkg::Use-Pty=0 -o=Acquire::ForceIPv4=true install build-essential \
+	libcurl4-gnutls-dev protobuf-compiler libboost-all-dev autotools-dev automake \
+	libboost-all-dev libssl-dev make autoconf libtool git apt-utils g++ \
+	libprotobuf-dev pkg-config libudev-dev libqrencode-dev bsdmainutils \
+	pkg-config libgmp3-dev libevent-dev jp2a pv virtualenv libdb4.8-dev libdb4.8++-dev
 	systemctl stop 'helium*' \
 	&& git clone https://github.com/heliumchain/helium.git \
 	&& cd helium \
