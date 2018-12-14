8621ccc2d5fcdf7d1a24c965eb736b4279cee307
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-14 15:47:46 +0000

    fix project reference

--- autoupdate/updatefromsource.sh
+++ autoupdate/updatefromsource.sh
@@ -3,8 +3,9 @@
 
 LOGFILE='/root/installtemp/autoupdate.log'
 echo -e "`date +%m.%d.%Y_%H:%M:%S` : Running updatefromsource.sh" | tee -a "$LOGFILE"
- 
 cd /root/installtemp
+INSTALLDIR='/root/installtemp'
+PROJECT=`cat $INSTALLDIR/vpscoin.info`
 GITAPI_URL="https://api.github.com/repos/heliumchain/helium/releases/latest"
 CURVERSION=`cat currentversion`
 NEWVERSION="$(curl -s $GITAPI_URL | grep tag_name)"
@@ -19,18 +20,18 @@ then echo -e "Installed version is $CURVERSION; new version detected: $NEWVERSIO
 	libboost-all-dev libssl-dev make autoconf libtool git apt-utils g++ \
 	libprotobuf-dev pkg-config libudev-dev libqrencode-dev bsdmainutils \
 	pkg-config libgmp3-dev libevent-dev jp2a pv virtualenv libdb4.8-dev libdb4.8++-dev
-	systemctl stop 'helium*' \
+	systemctl stop '$PROJECT*' \
 	&& git clone https://github.com/heliumchain/helium.git \
-	&& cd helium \
+	&& cd $PROJECT \
 	&& ./autogen.sh \
 	&& ./configure --disable-dependency-tracking --enable-tests=no --without-gui --without-miniupnpc --with-incompatible-bdb CFLAGS="-march=native" LIBS="-lcurl -lssl -lcrypto -lz" \
 	&& make \
 	&& make install \
-        && cd /usr/local/bin && rm -f !"("activate_masternodes_helium")" \
-	&& cp /root/installtemp/helium/src/{helium-cli,heliumd,helium-tx} /usr/local/bin/ \
-        && rm -rf root/installtemp/helium \
-        && cd /root/installtemp \
-        && curl -s $GITAPI_URL \
+	&& cd /usr/local/bin && rm -f !"("activate_masternodes_"$PROJECT"")" \
+	&& cp /root/installtemp/$PROJECT/src/{"$PROJECT"-cli,"$PROJECT"d,"$PROJECT"-tx} /usr/local/bin/ \
+	&& rm -rf root/installtemp/$PROJECT \
+	&& cd /root/installtemp \
+	&& curl -s $GITAPI_URL \
              | grep tag_name > currentversion \
-        && reboot
+	&& reboot
 fi
