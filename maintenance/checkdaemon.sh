494b59d66ba64e28b5a8cdcaa2d6b840349477c3
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-14 10:26:50 -0900

    Update checkdaemon.sh

--- maintenance/checkdaemon.sh
+++ maintenance/checkdaemon.sh
@@ -17,7 +17,7 @@ currentBlock=$(/usr/local/bin/"$PROJECT"-cli -conf=/etc/masternodes/"$PROJECT"_n
 /usr/local/bin/"$PROJECT"-cli -conf=/etc/masternodes/"$PROJECT"_n${i}.conf getblockcount > /root/installtemp/blockcount${i}
 if [ "$previousBlock$" == "$currentBlock$" ]; then
 	echo -e " Previous block is $previousBlock and current block is $currentBlock; same"
-	echo -e " `date +%Y.%m.%d" "%H:%M:%S` : Auto-restarting ${PROJECT}_n${i} because it seems stuck.\n"  | tee -a "$LOGFILE"
+	echo -e " `date +%m.%d.%Y_%H:%M:%S` : Auto-restarting ${PROJECT}_n${i} because it seems stuck.\n"  | tee -a "$LOGFILE"
         systemctl stop "$PROJECT"_n${i};
         sleep 10;
         systemctl start "$PROJECT"_n${i};
