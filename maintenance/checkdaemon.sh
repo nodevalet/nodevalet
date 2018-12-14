fd9dfa4ad3de9d71fe9604b46d2e27d16cbeebef
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-14 12:06:37 +0000

    Create checkdaemon.sh

---
+++ maintenance/checkdaemon.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# checkdaemon.sh
+# Make sure the daemon is not stuck.
+# Add the following to the crontab (i.e. crontab -e)
+# */30 * * * * /root/code-red/checkdaemon.sh
+
+INSTALLDIR='/root/installtemp'
+PROJECT=`cat $INSTALLDIR/vpscoin.info`
+
+previousBlock=$(cat /root/installtemp/blockcount)
+currentBlock=$(/usr/local/bin/$PROJECT-cli -conf=/etc/masternodes/$PROJECT_n1.conf getblockcount)
+
+/usr/local/bin/$PROJECT-cli -conf=/etc/masternodes/$PROJECT_n1.conf getblockcount > /root/installtemp/blockcount
+
+if [ "$previousBlock" == "$currentBlock" ]; then
+  	systemctl stop '$PROJECT*';
+  	sleep 10;
+  	cd /usr/local/bin; 
+	./activate_masternodes_$PROJECT;
+fi
