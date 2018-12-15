78b286606f68cd020b4adb042989bb8fb7fe1ce4
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-15 11:23:10 +0000

    added log entry

--- maintenance/makerun.sh
+++ maintenance/makerun.sh
@@ -2,13 +2,14 @@
 #compare nr. of running nodes to number of installed nodes. If different restart daemon
 # Add the following to the crontab (i.e. crontab -e)
 # */5 * * * * /root/code-red/maintenance/makerun.sh
-
+LOGFILE='/root/installtemp/makerun.log'
 INSTALLDIR='/root/installtemp'
 PROJECT=`cat $INSTALLDIR/vpscoin.info`
 TOTAL=`ps aux | grep -i "$PROJECT"d | wc -l`
 CUR_DAEMON=`expr $TOTAL - 1`
 EXP_DAEMON=`cat /root/installtemp/vpsnumber.info`
 if [ "$CUR_DAEMON" != "$EXP_DAEMON" ]
-  then cd /usr/local/bin
+  then echo -e " `date +%m.%d.%Y_%H:%M:%S` : I expected $EXP_DAEMON daemons but found only $CUR_DAEMON. Restarting... \n" | tee -a "$LOGFILE"
+  cd /usr/local/bin
   ./activate_masternodes_"$PROJECT"
 fi
