211977ddfde1474efd5a50989774658fe5a25a79
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-14 16:01:54 -0900

    Update rebootq.sh

--- maintenance/rebootq.sh
+++ maintenance/rebootq.sh
@@ -4,19 +4,20 @@
 # */30 * * * * /root/code-red/maintenance/rebootq.sh
 
 INSTALLDIR='/root/installtemp'
-LOGFILE='/root/installtemp/rebootq.log'
+LOGFILE='/root/installtemp/update-reboot.log'
 
 # if reqboot is required, write which packages require it
 cat /run/reboot* > $INSTALLDIR/REBOOTREQ
 
-if [ -s $INSTALLDIR/REBOOTREQ ]; then
-	# reboot server
+if [ -s $INSTALLDIR/REBOOTREQ ]
+then echo -e "`date +%m.%d.%Y_%H:%M:%S` : Reboot required to finish installing these updates" | tee -a "$LOGFILE"
+echo -e "`cat /run/reboot*" | tee -a "$LOGFILE"
+rm $INSTALLDIR/REBOOTREQ
+shutdown -r +5 "Restarting server to install updates"
+# reboot server
 	
-then 
 else 
+echo -e "No restart is required at this time"
 fi
-	
-
-rm /root/installtemp/rebootq
 
 exit
