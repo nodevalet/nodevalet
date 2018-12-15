0c5d352acb92be5cf62f87984bcddb18cf8324cf
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-15 11:12:00 +0000

    remove double entry

--- autoupdate/autoupdate.sh
+++ autoupdate/autoupdate.sh
@@ -2,10 +2,9 @@
 # to be added to crontab to run updatebinaries and, if that fails, run updatefromsource, if that fails, restarts daemon and try again tomorrow.
 
 LOGFILE='/root/installtemp/autoupdate.log'
+INSTALLDIR='/root/installtemp'
 PROJECT=`cat $INSTALLDIR/vpscoin.info`
 
 echo -e "`date +%m.%d.%Y_%H:%M:%S` : Autoupdate is looking for new $PROJECT tags." | tee -a "$LOGFILE"
 
-INSTALLDIR='/root/installtemp'
-PROJECT=`cat $INSTALLDIR/vpscoin.info`
 bash /root/code-red/autoupdate/updatebinaries.sh || bash /root/code-red/autoupdate/updatefromsource.sh || cd /usr/local/bin && ./activate_masternodes_"$PROJECT"
