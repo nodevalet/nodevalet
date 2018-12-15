43de8253b0473ba872d65344bcd19a5d73c4b9c4
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-14 22:36:26 -0900

    Update get-hard.sh

--- vps-harden/get-hard.sh
+++ vps-harden/get-hard.sh
@@ -1161,7 +1161,7 @@ begin_log
 curl -X POST https://www.heliumstats.online/code-red/status.php -H 'Content-Type: application/json-rpc' -d '{"hostname":"'"$HNAME"'","message": "Creating Swap Space..."}' && echo -e " "
 create_swap
 
-curl -X POST https://www.heliumstats.online/code-red/status.php -H 'Content-Type: application/json-rpc' -d '{"hostname":"'"$HNAME"'","message": "Updating Server ..."}' && echo -e " "
+curl -X POST https://www.heliumstats.online/code-red/status.php -H 'Content-Type: application/json-rpc' -d '{"hostname":"'"$HNAME"'","message": "Updating Server Software ..."}' && echo -e " "
 update_upgrade
 favored_packages
 # crypto_packages
