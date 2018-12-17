6210431f0069e80163a8c2482eab41cba9520fe1
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-17 07:24:24 -0900

    Update rebootq.sh

--- maintenance/rebootq.sh
+++ maintenance/rebootq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Check if reboot is required, and if so, reboot
 # Add the following to the crontab (i.e. crontab -e)
-# */30 * * * * /root/code-red/maintenance/rebootq.sh
+# (crontab -l ; echo "0 0 12 * * ? /root/code-red/maintenance/rebootq.sh") | crontab - 
 
 INSTALLDIR='/root/installtemp'
 LOGFILE='/root/installtemp/update-reboot.log'
@@ -16,16 +16,17 @@ then echo -e "`date +%m.%d.%Y_%H:%M:%S` : These updates require a reboot:" | tee
 
 # this sed removes the line "*** System restart required ***" from the REBOOTREQ
 sed -i '/restart required/d' $INSTALLDIR/REBOOTREQ
+
 # this echo writes the packages requiring reboot to the log
 echo -e "`cat ${INSTALLDIR}/REBOOTREQ`" | tee -a "$LOGFILE"
 
 rm $INSTALLDIR/REBOOTREQ
 echo -e "Server will restart in 5 minutes to complete required updates \n" | tee -a "$LOGFILE"
-
 shutdown -r +5 "Server will restart in 5 minutes to complete required updates"
 
 else
 echo -e "No reboot is required at this time\n" | tee -a "$LOGFILE"
+rm $INSTALLDIR/REBOOTREQ
 fi
 
 exit
