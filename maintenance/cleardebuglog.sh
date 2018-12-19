839a562d2d8e281e50745ea4ffcf0b914411dae5
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-19 14:49:17 -0900

    Update cleardebuglog.sh

--- maintenance/cleardebuglog.sh
+++ maintenance/cleardebuglog.sh
@@ -2,11 +2,10 @@
 # clearlog.sh
 # Clear debug.log every other day
 # Add the following to the crontab (i.e. crontab -e)
-# 0 0 */2 * * /root/code-red/maintenance/clearlog.sh
 
-INSTALLDIR='/root/installtemp'
-PROJECT=`cat $INSTALLDIR/vpscoin.info`
-MNS=`cat $INSTALLDIR/vpsnumber.info`
+INSTALLDIR='/var/temp/nodevalet'
+PROJECT=`cat $INSTALLDIR/info/vpscoin.info`
+MNS=`cat $INSTALLDIR/info/vpsnumber.info`
 
 for ((i=1;i<=$MNS;i++));
 do /bin/date > /var/lib/masternodes/"$PROJECT"${i}/debug.log
