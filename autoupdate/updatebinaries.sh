55b0fe0a9bf76011b8c16f92efbb219458a6e6e0
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-19 14:42:31 -0900

    Update updatebinaries.sh

--- autoupdate/updatebinaries.sh
+++ autoupdate/updatebinaries.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #check for updates and install binaries if necessary
 
-LOGFILE='/root/installtemp/autoupdate.log'
+LOGFILE='/var/temp/nodevalet/log/autoupdate.log'
 echo -e " `date +%m.%d.%Y_%H:%M:%S` : Running autoupdatebinaries.sh"  | tee -a "$LOGFILE"
-cd /root/installtemp
-INSTALLDIR='/root/installtemp'
-PROJECT=`cat $INSTALLDIR/vpscoin.info`
+cd /var/temp/nodevalet
+INSTALLDIR='/var/temp/nodevalet'
+PROJECT=`cat $INSTALLDIR/info/vpscoin.info`
 
 # Pull GITAPI_URL from $PROJECT.env
-GIT_API=`grep ^GITAPI_URL /root/code-red/nodemaster/config/${PROJECT}/${PROJECT}.env`
-echo "$GIT_API" > $INSTALLDIR/GIT_API
-sed -i "s/GITAPI_URL=//" $INSTALLDIR/GIT_API
-GITAPI_URL=$(<$INSTALLDIR/GIT_API)
+GIT_API=`grep ^GITAPI_URL $INSTALLDIR/nodemaster/config/${PROJECT}/${PROJECT}.env`
+echo "$GIT_API" > $INSTALLDIR/temp/GIT_API
+sed -i "s/GITAPI_URL=//" $INSTALLDIR/temp/GIT_API
+GITAPI_URL=$(<$INSTALLDIR/temp/GIT_API)
 
 # GITAPI_URL="https://api.github.com/repos/heliumchain/helium/releases/latest"
 CURVERSION=`cat currentversion`
@@ -20,7 +20,7 @@ if [ "$CURVERSION" != "$NEWVERSION" ]
 then echo -e " Installed version is : $CURVERSION" | tee -a "$LOGFILE"
      echo -e " New version detected : $NEWVERSION" | tee -a "$LOGFILE"
      echo -e " Attempting to install new binaries" | tee -a "$LOGFILE"
-		touch updating
+		touch $INSTALLDIR/temp/updating
 		systemctl stop $PROJECT* \
 		| curl -s $GITAPI_URL \
 		| grep browser_download_url \
