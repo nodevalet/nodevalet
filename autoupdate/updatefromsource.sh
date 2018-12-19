f8a527c6f1d3dbd1d6161b3e46df160cb0cef73f
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-19 14:44:50 -0900

    Update updatefromsource.sh

--- autoupdate/updatefromsource.sh
+++ autoupdate/updatefromsource.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #check for updates and build from source if installing binaries failed. 
 
-LOGFILE='/root/installtemp/autoupdate.log'
+LOGFILE='/var/temp/nodevalet/log/autoupdate.log'
 echo -e " `date +%m.%d.%Y_%H:%M:%S` : Running updatefromsource.sh" | tee -a "$LOGFILE"
 cd /root/installtemp
-INSTALLDIR='/root/installtemp'
-PROJECT=`cat $INSTALLDIR/vpscoin.info`
+INSTALLDIR='/var/temp/nodevalet'
+PROJECT=`cat $INSTALLDIR/info/vpscoin.info`
 
 # Pull GITAPI_URL from $PROJECT.env
-GIT_API=`grep ^GITAPI_URL /root/code-red/nodemaster/config/$PROJECT/$PROJECT.env`
-echo "$GIT_API" > $INSTALLDIR/GIT_API
-sed -i "s/GITAPI_URL=//" $INSTALLDIR/GIT_API
-GITAPI_URL=$(<$INSTALLDIR/GIT_API)
+GIT_API=`grep ^GITAPI_URL $INSTALLDIR/nodemaster/config/$PROJECT/$PROJECT.env`
+echo "$GIT_API" > $INSTALLDIR/temp/GIT_API
+sed -i "s/GITAPI_URL=//" $INSTALLDIR/temp/GIT_API
+GITAPI_URL=$(<$INSTALLDIR/temp/GIT_API)
 
 # GITAPI_URL="https://api.github.com/repos/heliumchain/helium/releases/latest"
 CURVERSION=`cat currentversion`
@@ -34,9 +34,9 @@ then 	echo -e " I couldn't download the new binaries, so I am now" | tee -a "$LO
 	&& make \
 	&& make install \
 	&& cd /usr/local/bin && rm -f !"("activate_masternodes_"$PROJECT"")" \
-	&& cp /root/installtemp/$PROJECT/src/{"$PROJECT"-cli,"$PROJECT"d,"$PROJECT"-tx} /usr/local/bin/ \
-	&& rm -rf root/installtemp/$PROJECT \
-	&& cd /root/installtemp \
+	&& cp /var/temp/nodevalet/$PROJECT/src/{"$PROJECT"-cli,"$PROJECT"d,"$PROJECT"-tx} /usr/local/bin/ \
+	&& rm -rf /var/temp/nodevalet/$PROJECT \
+	&& cd /var/temp/nodevalet/temp \
 	&& rm -f updating \
 	&& curl -s $GITAPI_URL \
 		| grep tag_name > currentversion \
