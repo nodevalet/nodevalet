f99d6c22a83cdd8c2e92914a7173eab8be96793e
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-19 16:28:03 -0900

    Update checkdaemon.sh

--- maintenance/checkdaemon.sh
+++ maintenance/checkdaemon.sh
@@ -3,12 +3,12 @@
 # Make sure the daemon is not stuck.
 # Add the following to the crontab (i.e. crontab -e)
 
-INSTALLDIR='/var/temp/nodevalet'
+INSTALLDIR='/var/tmp/nodevalet'
 PROJECT=`cat $INSTALLDIR/info/vpscoin.info`
 MNS=`cat $INSTALLDIR/info/vpsnumber.info`
 LOGFILE='$INSTALLDIR/log/checkdaemon.log'
 
-if [ -e "$INSTALLDIR/temp/updating ]
+if [ -e "$INSTALLDIR/tmp/updating ]
 	then echo "Looks like I'm installing updates, I'll try again later."  | tee -a "$LOGFILE"
 	exit
 fi
@@ -16,9 +16,9 @@ fi
 for ((i=1;i<=$MNS;i++));
 do
 echo -e " Checking for stuck blocks on masternode "$PROJECT"_n${i}"
-previousBlock=`cat $INSTALLDIR/temp/blockcount${i}`
+previousBlock=`cat $INSTALLDIR/tmp/blockcount${i}`
 currentBlock=$(/usr/local/bin/"$PROJECT"-cli -conf=/etc/masternodes/"$PROJECT"_n${i}.conf getblockcount)
-/usr/local/bin/"$PROJECT"-cli -conf=/etc/masternodes/"$PROJECT"_n${i}.conf getblockcount > $INSTALLDIR/temp/blockcount${i}
+/usr/local/bin/"$PROJECT"-cli -conf=/etc/masternodes/"$PROJECT"_n${i}.conf getblockcount > $INSTALLDIR/tmp/blockcount${i}
 if [ "$previousBlock$" == "$currentBlock$" ]; then
 	echo -e " Previous block is $previousBlock and current block is $currentBlock; same"
 	echo -e " `date +%m.%d.%Y_%H:%M:%S` : Auto-restarting ${PROJECT}_n${i} because it seems stuck.\n"  | tee -a "$LOGFILE"
