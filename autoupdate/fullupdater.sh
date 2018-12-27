c691e566fdca65276c1bdffff42e042e2e784eb7
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-27 15:16:28 +0000

    corrected dir

--- autoupdate/fullupdater.sh
+++ autoupdate/fullupdater.sh
@@ -88,8 +88,8 @@ then 	echo -e " I couldn't download the new binaries, so I am now"
 	make
 	make install
 	cd /usr/local/bin && rm -f !"("activate_masternodes_"$PROJECT"")"
-	cp $INSTALLDIR/$PROJECT/src/{"$PROJECT"-cli,"$PROJECT"d,"$PROJECT"-tx} /usr/local/bin/
-	rm -rf $INSTALLDIR/$PROJECT
+	cp $INSTALLDIR/$PROJECT/temp/src/{"$PROJECT"-cli,"$PROJECT"d,"$PROJECT"-tx} /usr/local/bin/
+	rm -rf $INSTALLDIR/temp/$PROJECT
 	cd $INSTALLDIR/temp
 	echo -e " Starting masternodes after building ${PROJECTt} from source" >> "$LOGFILE"
 	activate_masternodes_$PROJECT
