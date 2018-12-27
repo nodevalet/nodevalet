2468152b503aabbb256c5de4a0a04d123bf122ef
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-12-26 17:54:32 -0900

    add logging

--- maintenance/makerun.sh
+++ maintenance/makerun.sh
@@ -8,6 +8,9 @@ INFODIR='/var/tmp/nvtemp'
 PROJECT=`cat $INFODIR/vpscoin.info`
 MNS=`cat $INFODIR/vpsnumber.info`
 
+# add logging to check if cron is working as planned
+echo -e "`date +%m.%d.%Y_%H:%M:%S` : Executing makerun.sh (every 5 minutes, cron) \n"  | tee -a "$LOGFILE"
+
 TOTAL=`ps aux | grep -i "$PROJECT"d | wc -l`
 CUR_DAEMON=`expr $TOTAL - 1`
 EXP_DAEMON=`cat /var/temp/nodevalet/info/vpsnumber.info`
