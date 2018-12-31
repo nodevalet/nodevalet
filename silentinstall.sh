cef2da43514451b8137710de8d96eddb9162c293
TeelaBrown <45015269+TeelaBrown@users.noreply.github.com>
2018-12-31 01:11:57 +0000

    sort out path for autoupdate cron

--- silentinstall.sh
+++ silentinstall.sh
@@ -226,7 +226,7 @@ echo -e "  --> Check for stuck blocks every 30 minutes"  | tee -a "$LOGFILE"
 echo -e "  --> Check for & reboot if needed to install updates every 10 hours"  | tee -a "$LOGFILE"
 	(crontab -l ; echo "59 */10 * * * $INSTALLDIR/maintenance/rebootq.sh") | crontab -
 echo -e "  --> Check for wallet updates every 12 hours"  | tee -a "$LOGFILE"
-	(crontab -l ; echo "2 */12 * * * $INSTALLDIR/autoupdate/fullupdater.sh") | crontab -
+	(crontab -l ; echo "2 */12 * * * $INSTALLDIR/maintenance/autoupdate.sh") | crontab -
 echo -e "  --> Clear daemon debug logs weekly to prevent clog \n"  | tee -a "$LOGFILE"
 	(crontab -l ; echo "@weekly $INSTALLDIR/maintenance/cleardebuglog.sh") | crontab -
 
