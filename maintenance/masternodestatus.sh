e154fbc0afb12af669884eec5aac920d5e5382f3
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2019-01-05 11:22:56 -0900

    accept argument for masternodestatus

--- maintenance/masternodestatus.sh
+++ maintenance/masternodestatus.sh
@@ -24,6 +24,30 @@ if [ -e "$INSTALLDIR/temp/updating" ]
 	exit
 fi
 
+# read first argument to string
+input=$1
+
+if [ -z $input ] ; then :
+else
+while :; do
+	 
+	if [ -z $input ] ; then read -p "  --> " input ; fi
+	[[ $input =~ ^[0-9]+$ ]] || { printf "${lightred}";echo -e "\n I'm sorry; but I only recognize numbers of masternodes on this VPS.\n Which masternode would you like to see masternode status for? \n"; input=""; continue; }
+	if (($input >= 1 && $input <= $MNS)); then break
+	else echo -e "\n --> Can't find masternode $input, try again. \n"
+	input=""
+	fi
+done
+
+# Display 'masternode status' for only the masternode named
+echo -e "\n `date +%m.%d.%Y_%H:%M:%S` : Checking masternode status of ${PROJECT}_n${input}"
+MNSTATUS=`/usr/local/bin/${MNODE_DAEMON::-1}-cli -conf=/etc/masternodes/${PROJECT}_n${input}.conf masternode status`
+echo -e "$MNSTATUS"
+exit
+
+fi
+
+# Display 'masternode status' for all masternodes
 for ((i=1;i<=$MNS;i++)); 
 do
 
