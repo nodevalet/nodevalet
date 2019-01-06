aeb8440d584affa01eb84b9e27f2fab437d62b07
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2019-01-05 20:01:15 -0900

    adjust blank spaces in logging

--- maintenance/makerun.sh
+++ maintenance/makerun.sh
@@ -17,7 +17,6 @@ cat $INSTALLDIR/temp/MNODE_DAEMON | tr -d '[}]' > $INSTALLDIR/temp/MNODE_DAEMON1
 MNODE_DAEMON=$(<$INSTALLDIR/temp/MNODE_DAEMON1)
 cat $INSTALLDIR/temp/MNODE_DAEMON1 > $INSTALLDIR/temp/MNODE_DAEMON ; rm $INSTALLDIR/temp/MNODE_DAEMON1
 
-
 # add logging to check if cron is working as planned
 # echo -e "`date +%m.%d.%Y_%H:%M:%S` : Executing makerun.sh (every 5 minutes, cron) \n"  | tee -a "$LOGFILE"
 
@@ -27,12 +26,12 @@ EXP_DAEMON=`cat $INFODIR/vpsnumber.info`
 
 if [ -e $INSTALLDIR/temp/updating ]
 	then echo -e "`date +%m.%d.%Y_%H:%M:%S` : Running makerun.sh" | tee -a "$LOGFILE"
-	echo -e "It looks like I'm busy with something else; skipping make run.\n"  | tee -a "$LOGFILE"
+	echo -e " It looks like I'm busy with something else; skipping make run.\n"  | tee -a "$LOGFILE"
 	exit
 fi
 
 if [ "$CUR_DAEMON" != "$EXP_DAEMON" ]
-  then echo -e " `date +%m.%d.%Y_%H:%M:%S` : I expected $EXP_DAEMON daemons but found only $CUR_DAEMON. Restarting... \n" | tee -a "$LOGFILE"
+  then echo -e "`date +%m.%d.%Y_%H:%M:%S` : I expected $EXP_DAEMON daemons but found only $CUR_DAEMON. Restarting... \n" | tee -a "$LOGFILE"
   bash /usr/local/bin/activate_masternodes_"$PROJECT"
   else echo -e "\n `date +%m.%d.%Y_%H:%M:%S` : Found $CUR_DAEMON of $EXP_DAEMON expected daemons. All is well. \n"
 fi
