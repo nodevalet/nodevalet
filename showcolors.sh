72bf5ee612899b12877cdb8ee2e4f4de5c96a839
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2019-10-20 12:24:26 -0800

    Update showcolors.sh

--- showcolors.sh
+++ showcolors.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
     ### define colors ###
-    lightred='\033[1;31m'  # light red
-    red='\033[0;31m'  # red
-    lightgreen='\033[1;32m'  # light green
-    green='\033[0;32m'  # green
-    lightblue='\033[1;34m'  # light blue
-    blue='\033[0;34m'  # blue
-    lightpurple='\033[1;35m'  # light purple
-    purple='\033[0;35m'  # purple
-    lightcyan='\033[1;36m'  # light cyan
-    cyan='\033[0;36m'  # cyan
-    lightgray='\033[0;37m'  # light gray
-    white='\033[1;37m'  # white
-    brown='\033[0;33m'  # brown
-    yellow='\033[1;33m'  # yellow
-    darkgray='\033[1;30m'  # dark gray
-    black='\033[0;30m'  # black
-    nocolor='\033[0m' # no color
+    lightred=$'\033[1;31m'  # light red
+    red=$'\033[0;31m'  # red
+    lightgreen=$'\033[1;32m'  # light green
+    green=$'\033[0;32m'  # green
+    lightblue=$'\033[1;34m'  # light blue
+    blue=$'\033[0;34m'  # blue
+    lightpurple=$'\033[1;35m'  # light purple
+    purple=$'\033[0;35m'  # purple
+    lightcyan=$'\033[1;36m'  # light cyan
+    cyan=$'\033[0;36m'  # cyan
+    lightgray=$'\033[0;37m'  # light gray
+    white=$'\033[1;37m'  # white
+    brown=$'\033[0;33m'  # brown
+    yellow=$'\033[1;33m'  # yellow
+    darkgray=$'\033[1;30m'  # dark gray
+    black=$'\033[0;30m'  # black
+    nocolor=$'\e[0m' # no color
 
 # Used this while testing color output
 clear
@@ -33,15 +33,12 @@ printf " ${lightcyan}Light Cyan${nocolor}\n"
 printf " ${cyan}Cyan${nocolor}\n"
 printf " ${lightgray}Light Gray${nocolor}\n"
 printf " ${white}White${nocolor}\n"
-printf " ${lightbrown}Brown${nocolor}\n"
+printf " ${brown}Brown${nocolor}\n"
 printf " ${yellow}Yellow${nocolor}\n"
 printf " ${darkgray}Dark Gray${nocolor}\n"
 printf " ${black}Black${nocolor}\n"
-printf " ${yellow}Yellow${nocolor}\n"
+printf " ${cyan}Cyan\n"
 figlet " hello $(whoami)" -f small
+printf "Text in ${red}red${nocolor}, ${white}white${nocolor} and ${blue}blue${nocolor}.\n"
 printf " ${nocolor}${nocolor}\n"
-
-
-
-
 
