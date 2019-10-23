1666114b0e0f9a63ad62d5471ad71f8d79be0e1c
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2019-10-22 17:46:21 -0800

    Update silentinstall.sh

--- silentinstall.sh
+++ silentinstall.sh
@@ -654,12 +654,12 @@ function install_binaries() {
     fi
 
     # check if binaries already exist, skip installing crypto packages if they aren't needed
-        echo -e "Test for presence of binaries "  | tee -a "$LOGFILE"
-        echo -e "MNODE_DAEMON variable is currently set to ${MNODE_DAEMON} "  | tee -a "$LOGFILE"
+        # echo -e "Test for presence of binaries "  | tee -a "$LOGFILE"
+        # echo -e "MNODE_DAEMON variable is currently set to ${MNODE_DAEMON} "  | tee -a "$LOGFILE"
 
 
     dEXIST=$(ls /usr/local/bin | grep "${MNODE_DAEMON}")
-    echo -e "dEXIST variable is currently set to ${dEXIST} "  | tee -a "$LOGFILE"
+    # echo -e "dEXIST variable is currently set to ${dEXIST} "  | tee -a "$LOGFILE"
 
     # if [[ "${dEXIST}" == "${MNODE_DAEMON}" ]]  ; testing without this line
     if [[ "${dEXIST}" ]]
@@ -669,7 +669,7 @@ function install_binaries() {
             | grep tag_name > $INSTALLDIR/temp/currentversion
 
     else echo -e "Binaries for ${PROJECTt} could not be downloaded \n"  | tee -a "$LOGFILE"
-    echo -e "${dEXIST} (dEXIST) was not found to be equal to ${MNODE_DAEMON} (MNODE_DAEMON)"  | tee -a "$LOGFILE"
+        echo -e "${dEXIST} (dEXIST) was not found to be equal to ${MNODE_DAEMON} (MNODE_DAEMON)"  | tee -a "$LOGFILE"
     fi
 }
 
