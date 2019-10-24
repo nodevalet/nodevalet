0245fa2ccf44601eb952e060a6f6922090b43c98
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2019-10-24 07:29:23 -0800

    fix some spaces

--- maintenance/mulligan.sh
+++ maintenance/mulligan.sh
@@ -40,7 +40,7 @@ function search_and_destroy() {
     echo -e " This scriptlet will disable your masternodes on this VPS and "
     echo -e " destroy all NodeValet data. It is intended for testing only.\n"
 
-    echo -e " DO NOT USE THIS IN PRODUCTION UNLESS YOU REALLY MEAN TO."
+    echo -e " ** DO NOT USE THIS IN PRODUCTION UNLESS YOU REALLY MEAN TO **"
     # echo -e -n "${cyan}"
     while :; do
         echo -e "\n"
@@ -72,19 +72,19 @@ function search_and_destroy() {
         echo -e " $(date +%m.%d.%Y_%H:%M:%S) : Stopping and disabling masternodes"
         echo -e "---------------------------------------------------------- ${white}\n"
 
-        echo -e "${yellow}-------------------------------------------------------------- "
+        echo -e "${yellow}------------------------------------------------------------------ "
         for ((i=1;i<=$MNS;i++));
         do
             echo -e " $(date +%m.%d.%Y_%H:%M:%S) : Stopping and disabling masternode ${PROJECT}_n${i}"
             systemctl disable "${PROJECT}"_n${i}
             systemctl stop "${PROJECT}"_n${i}
         done
-        echo -e "-------------------------------------------------------------------- ${white}\n"
+        echo -e "------------------------------------------------------------------------------ ${white}\n"
         sleep 2
 
-        echo -e "${yellow}------------------------------------------------------------------- "
+        echo -e "${yellow}-------------------------------------------------------------------- "
         echo -e " $(date +%m.%d.%Y_%H:%M:%S) : Removing all masternodes and blockchain data"
-        echo -e "------------------------------------------------------------------- ${white}\n"
+        echo -e "-------------------------------------------------------------------- ${white}\n"
         rm -rf /var/lib/masternodes
 
         echo -e "${lightgreen}----------------------------------------------------------------------------- "
@@ -106,9 +106,9 @@ function search_and_destroy() {
         echo -e "------------------------------------------------------------------ ${white}\n"
         sudo rm -rf /var/tmp/nodevalet
 
-        echo -e "${lightgreen}------------------------------------------------------------------------ "
+        echo -e "${lightgreen}------------------------------------------------------------------------- "
         echo " $(date +%m.%d.%Y_%H:%M:%S) : SUCCESS : NodeValet was scrubbed from this Server"
-        echo -e "------------------------------------------------------------------------ ${nocolor}\n"
+        echo -e "------------------------------------------------------------------------- ${nocolor}\n"
 
     else :
         echo -e "${yellow}---------------------------------------------------- "
