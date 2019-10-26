5717b0b37cd8c23c0e82c95ab018b10557c3d407
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2019-10-26 09:46:37 -0800

    Update clonesync.sh

--- maintenance/clonesync.sh
+++ maintenance/clonesync.sh
@@ -43,7 +43,7 @@ if [[ "${TARGETSYNC}" ]]
 then echo -e "* ${PROJECT}_n${t} is already synced *\n"
 rm -rf $INSTALLDIR/temp/updating
 exit
-else echo -e "${PROJECT}_n${t} is not synced and will be clonesynced" | tee -a "$LOGFILE"
+else echo -e " Masternode ${PROJECT}_n${t} is not synced and will be clonesynced\n"
 fi
 
 # Search for first fully-synced masternode and assign that number to $s
@@ -57,15 +57,15 @@ do
 
     SOURCESYNC=$(ls /var/tmp/nodevalet/temp | grep "${PROJECT}_n${i}" | grep "synced")
     if [[ "${i}" == "${t}" ]]
-    then echo -e " ${PROJECT}_n${i} is Target; cannot be Source.\n"
+    then echo -e " Masternode ${PROJECT}_n${i} is Target; cannot be Source.\n"
 
     elif [[ "${SOURCESYNC}" ]]
-    then echo -e " ${PROJECT}_n${i} is synced and a valid Source Masternode\n"
+    then echo -e " Masternode ${PROJECT}_n${i} is synced and a valid Source Masternode.\n"
     s=$i
     echo -e "Setting Source Masternode to $i"
     break
 
-    else echo -e "${PROJECT}_n${t} is not a valid source; I will try another if it exists."
+    else echo -e " Masternode ${PROJECT}_n${t} is not synced so it is not a valid source.\n"
     fi
 
 done
@@ -79,54 +79,46 @@ if [[ "${s}" == "0" ]]
 else echo -e " Clonesync will now attempt to clone the blockchain from "
     echo -e " ${PROJECT}_n${s} to ${PROJECT}_n${t}. Good luck."
 
+# echo -e "This is the part where I would do the things."
+# echo -e "Exiting because I haven't written the rest of the code yet."
+# rm -rf $INSTALLDIR/temp/updating
+# exit
+
+echo -e "\n $(date +%m.%d.%Y_%H:%M:%S) : Running clonesync.sh" | tee -a "$LOGFILE"
+echo -e " Going to clone ${PROJECT}_n${s}'s blockchain onto ${PROJECT}_n${t}."  | tee -a "$LOGFILE"
 
 
-echo -e "This is the part where I would do the things."
-echo -e "Exiting because I haven't written the rest of the code yet."
-rm -rf $INSTALLDIR/temp/updating
-exit
-
-
-
-
-echo -e "\n"
-echo -e "$(date +%m.%d.%Y_%H:%M:%S) : Running clonesync.sh" | tee -a "$LOGFILE"
-echo -e " Going to clone ${PROJECT}_n${s}'s blockchain onto ${PROJECT}_n${t}'.\n"  | tee -a "$LOGFILE"
-
-
-echo -e " Disabling Source Masternode ${PROJECT}_n${s} now."
+echo -e "\n Disabling Source Masternode ${PROJECT}_n${s} now."
 sudo systemctl disable "${PROJECT}"_n${s}
 sudo systemctl stop "${PROJECT}"_n${s}
 
-echo -e " Disabling Target Masternode ${PROJECT}_n${t} now."
+echo -e "\n Disabling Target Masternode ${PROJECT}_n${t} now."
 sudo systemctl disable "${PROJECT}"_n${t}
 sudo systemctl stop "${PROJECT}"_n${t}
 sleep 2
 
-echo -e " Removing target blockchain data except wallet.dat and masternode.conf."
+echo -e "\n Removing target blockchain data except wallet.dat and masternode.conf."
 cd /var/lib/masternodes/"${PROJECT}"${t}
 sudo rm -rf !("wallet.dat"|"masternode.conf")
 sleep 2
 
-echo -e " Copying source blockchain data from MNx except wallet.dat and masternode.conf."
-# cp /var/lib/masternodes/"${PROJECT}"${t}/
-# cp !(b*) new_dir/  # this is a sample
+echo -e "\n Copying source blockchain data from MNx except wallet.dat and masternode.conf."
 
-cp !("wallet.dat"|"masternode.conf") new_dir/  # this is a sample
+cd /var/lib/masternodes/"${PROJECT}"${s}
+cp -r !("wallet.dat"|"${PROJECT}.pid"|"$backups") /var/lib/masternodes/"${PROJECT}"${t}/
+# cp -r !("wallet.dat"|"masternode.conf") /var/lib/masternodes/"${PROJECT}"${t}/
 
-sudo rm -rf !("wallet.dat"|"masternode.conf")
-
-echo -e " Restarting Source Masternode ${PROJECT}_n${s}.\n"
+echo -e "\n Restarting Source Masternode ${PROJECT}_n${s}."
 sudo systemctl enable "${PROJECT}"_n${s}
 sudo systemctl start "${PROJECT}"_n${s}
 
 
-echo -e " Restarting Target Masternode ${PROJECT}_n${t}.\n"
+echo -e "\n Restarting Target Masternode ${PROJECT}_n${t}."
 sudo systemctl enable "${PROJECT}"_n${t}
 sudo systemctl start "${PROJECT}"_n${t}
 
 
-echo -e " Clonesync complete.\n"
+echo -e " Clonesync complete; masternodes have been restarted.\n"  | tee -a "$LOGFILE"
 
 # echo -e " Unsetting -update flag \n"
 rm -f $INSTALLDIR/temp/updating
