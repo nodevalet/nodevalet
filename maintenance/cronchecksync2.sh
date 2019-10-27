2a0ede1ffc46dd3bf9d0d92bc789febfa1f37945
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2019-10-27 21:59:59 +0000

    Update cronchecksync2.sh

--- maintenance/cronchecksync2.sh
+++ maintenance/cronchecksync2.sh
@@ -85,13 +85,13 @@ function check_blocksync() {
 
     while [ $SECONDS -lt $end ]; do
         # echo -e "Time $SECONDS"
-        rm -rf $INSTALLDIR/getinfo_n1
-        touch $INSTALLDIR/getinfo_n1
-        /usr/local/bin/"${MNODE_DAEMON::-1}"-cli -conf=/etc/masternodes/"${PROJECT}_n${i}".conf getinfo > $INSTALLDIR/getinfo_n1
+        rm -rf $INSTALLDIR/getinfo_n${i}
+        touch $INSTALLDIR/getinfo_n${i}
+        /usr/local/bin/"${MNODE_DAEMON::-1}"-cli -conf=/etc/masternodes/"${PROJECT}_n${i}".conf getinfo > $INSTALLDIR/getinfo_n${i}
 
         # if  masternode not running, echo masternode not running and break
-        BLOCKS=$(grep "blocks" $INSTALLDIR/getinfo_n1 | tr -dc '0-9')
-        # CONNECTIONS=$(grep "connections" $INSTALLDIR/getinfo_n1 | tr -dc '0-9')
+        BLOCKS=$(grep "blocks" $INSTALLDIR/getinfo_n${i} | tr -dc '0-9')
+        # CONNECTIONS=$(grep "connections" $INSTALLDIR/getinfo_n${i} | tr -dc '0-9')
         echo -e "\n${lightcyan}    --> $PROJECTt Masternode $i Sync Status <-- ${nocolor}\n"
 
         # echo -e "${white} Masternode n$i is currently synced through block: ${lightpurple}$BLOCKS${nocolor}\n"
