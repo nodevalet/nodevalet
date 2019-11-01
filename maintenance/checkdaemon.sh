6638ee388a250b300e7fbd59bd001cd2b3cd1054
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2019-11-01 00:41:29 +0000

    Update checkdaemon.sh

--- maintenance/checkdaemon.sh
+++ maintenance/checkdaemon.sh
@@ -39,7 +39,7 @@ do
     if [ "$previousBlock$" == "$currentBlock$" ]
     then
         echo -e " Previous block is $previousBlock and current block is $currentBlock; same"
-        echo -e "$(date +%m.%d.%Y_%H:%M:%S) : Auto-restarting ${PROJECT}_n${i} because it seems stuck.\n"  | tee -a "$LOGFILE"
+        echo -e " $(date +%m.%d.%Y_%H:%M:%S) : Auto-restarting ${PROJECT}_n${i} because it seems stuck.\n"  | tee -a "$LOGFILE"
         systemctl stop "${PROJECT}"_n${i}
         sleep 5
         systemctl start "${PROJECT}"_n${i}
