926dd00be7e464cfeb5e74a6bc6fcfca0d203f9e
akcryptoguy <45030509+akcryptoguy@users.noreply.github.com>
2020-03-09 00:07:43 -0800

    outsource

--- maintenance/addmn.sh
+++ maintenance/addmn.sh
@@ -300,7 +300,9 @@ function sub_genkeys() {
 function start_mns() {
         for ((i=($MNS+1);i<=$TNODES;i++));
         do
+        touch $INSTALLDIR/temp/gettinginfo
         clonesync $i
+        rm $INSTALLDIR/temp/gettinginfo --force
         # display countdown timer on screen
         echo -e " Please wait patiently while the masternodes start-- do not interrupt!"
         seconds=90; date1=$((`date +%s` + $seconds));
@@ -475,26 +477,12 @@ EOT
 
 function remove_crons() {
     # disable the crons that could cause problems
-    crontab -l | grep -v '/var/tmp/nodevalet/maintenance/rebootq.sh'  | crontab -
-    crontab -l | grep -v '/var/tmp/nodevalet/maintenance/makerun.sh'  | crontab -
-    crontab -l | grep -v '/var/tmp/nodevalet/maintenance/checkdaemon.sh'  | crontab -
-    crontab -l | grep -v '/var/tmp/nodevalet/maintenance/autoupdate.sh'  | crontab -
-    crontab -l | grep -v '/var/tmp/nodevalet/maintenance/cronchecksync1.sh'  | crontab -
+    . /var/tmp/nodevalet/maintenance/remove_crons.sh
 }
 
 function restore_crons() {
     # restore maintenance crons that were previously disabled
-    echo -e "${yellow} Re-enabling crontabs that were previously disabled:${nocolor}"
-    echo -e "${white}  --> Check for & reboot if needed to install updates every 10 hours${nocolor}"
-    (crontab -l ; echo "59 */10 * * * /var/tmp/nodevalet/maintenance/rebootq.sh") | crontab -
-    echo -e "${white}  --> Make sure all daemons are running every 10 minutes${nocolor}"
-    (crontab -l ; echo "7,17,27,37,47,57 * * * * /var/tmp/nodevalet/maintenance/makerun.sh") | crontab -
-    echo -e "${white}  --> Check for stuck blocks every 30 minutes${nocolor}"
-    (crontab -l ; echo "1,31 * * * * /var/tmp/nodevalet/maintenance/checkdaemon.sh") | crontab -
-    echo -e "${white}  --> Check for wallet updates every 48 hours${nocolor}"
-    (crontab -l ; echo "2 */48 * * * /var/tmp/nodevalet/maintenance/autoupdate.sh") | crontab -
-    echo -e "${white}  --> Check if chains are syncing or synced every 5 minutes${nocolor}"
-    (crontab -l ; echo "*/5 * * * * /var/tmp/nodevalet/maintenance/cronchecksync1.sh") | crontab -
+    . /var/tmp/nodevalet/maintenance/restore_crons.sh
 }
 
 # This is where the script actually starts
---
+++ maintenance/remove_crons.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# disable the crons that could cause problems with other scripts
+
+crontab -l | grep -v '/var/tmp/nodevalet/maintenance/rebootq.sh'  | crontab -
+crontab -l | grep -v '/var/tmp/nodevalet/maintenance/makerun.sh'  | crontab -
+crontab -l | grep -v '/var/tmp/nodevalet/maintenance/checkdaemon.sh'  | crontab -
+crontab -l | grep -v '/var/tmp/nodevalet/maintenance/autoupdate.sh'  | crontab -
+crontab -l | grep -v '/var/tmp/nodevalet/maintenance/cronchecksync1.sh'  | crontab -
