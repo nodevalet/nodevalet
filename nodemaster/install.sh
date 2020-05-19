dc052e5cf3521c3009ff9618d1deb0a1894609f3
akcryptoguy <akcryptoguy+github@gmail.com>
2020-05-18 19:34:55 -0800

    optimize pngs

--- nodemaster/install.sh
+++ nodemaster/install.sh
@@ -10,6 +10,25 @@ MNODE_BINARIES=$(<$INFODIR/vpsbinaries.info)
 HNAME=$(<$INFODIR/vpshostname.info)
 PROJECT=$(<$INFODIR/vpscoin.info)
 
+### define colors ###
+lightred=$'\033[1;31m'  # light red
+red=$'\033[0;31m'  # red
+lightgreen=$'\033[1;32m'  # light green
+green=$'\033[0;32m'  # green
+lightblue=$'\033[1;34m'  # light blue
+blue=$'\033[0;34m'  # blue
+lightpurple=$'\033[1;35m'  # light purple
+purple=$'\033[0;35m'  # purple
+lightcyan=$'\033[1;36m'  # light cyan
+cyan=$'\033[0;36m'  # cyan
+lightgray=$'\033[0;37m'  # light gray
+white=$'\033[1;37m'  # white
+brown=$'\033[0;33m'  # brown
+yellow=$'\033[1;33m'  # yellow
+darkgray=$'\033[1;30m'  # dark gray
+black=$'\033[0;30m'  # black
+nocolor=$'\e[0m' # no color
+
 # This script was copied, modified, bastardized, improved, and wholly wrecked by Node Valet
 #  ███╗   ██╗ ██████╗ ██████╗ ███████╗███╗   ███╗ █████╗ ███████╗████████╗███████╗██████╗
 #  ████╗  ██║██╔═══██╗██╔══██╗██╔════╝████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗
@@ -134,7 +153,6 @@ function install_packages() {
     if [[ "${dEXISTT}" ]]
     then echo -e "${lightcyan}Binaries for ${PROJECT} already exist, no need to download crypto packages${nocolor}\n"   | tee -a ${SCRIPT_LOGFILE}
     else echo -e "${lightred}Did not find binaries for ${PROJECT} so downloading crypto packages${nocolor}"  | tee -a ${SCRIPT_LOGFILE}
-        echo -e "${lightred}${dEXIST} (dEXIST) was not found to exist${nocolor}\n"  | tee -a ${SCRIPT_LOGFILE}
         if [ -e $INFODIR/fullauto.info ] ; then curl -X POST https://www.nodevalet.io/status.php -H 'Content-Type: application/json-rpc' -d '{"hostname":"'"$HNAME"'","message": "Server is now installing crypto packages because proper binaries could not be located ..."}' && echo -e " " ; fi
         # development and build packages
         # these are common on all cryptos
